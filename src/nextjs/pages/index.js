4e1472c8893a7224cb19045fa74858becf21bfa8
Bunyut <bunyut.p@gmail.com>
2021-11-09 18:37:32 +0700

    Update index.js
    update

--- src/nextjs/pages/index.js
+++ src/nextjs/pages/index.js
@@ -13,47 +13,8 @@ export default function Home(props) {
       <Head>
         <title>LIFF Starter</title>
       </Head>
-      <div className="home">
-        <h1 className="home__title">
-          Welcome to <br />
-          <a
-            className="home__title__link"
-            href="https://developers.line.biz/en/docs/liff/overview/"
-          >
-            LIFF Starter!
-          </a>
-        </h1>
-        <div className="home__badges">
-          <span className="home__badges__badge badge--primary">LIFF Starter</span>
-          <span className="home__badges__badge badge--secondary">nextjs</span>
-          <span className="home__badges__badge badge--tertiary">{packageJson.version}</span>
-        </div>
-        <div className="home__buttons">
-          <a
-            href="https://developers.line.biz/en/docs/liff/developing-liff-apps/"
-            target="_blank"
-            rel="noreferrer"
-            className="home__buttons__button button--primary"
-          >
-            LIFF Documentation
-          </a>
-          <a
-            href="https://liff-playground.netlify.app/"
-            target="_blank"
-            rel="noreferrer"
-            className="home__buttons__button button--tertiary"
-          >
-            LIFF Playground
-          </a>
-          <a
-            href="https://developers.line.biz/console/"
-            target="_blank"
-            rel="noreferrer"
-            className="home__buttons__button button--secondary"
-          >
-            LINE Developers Console
-          </a>
-        </div>
+      <div>
+    <h1>Hello</h1>
       </div>
     </div>
   );
